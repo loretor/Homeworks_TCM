db476590375f1ae5f2fdc9f67b8329e0056fff92
Lorenzo Torri <68181508+loretor@users.noreply.github.com>
2022-05-16 17:30:37 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
+import './Button.dart';
+import 'NavBar.dart';
+import 'Options.dart';
+import 'Field.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          backgroundColor: Color(0xFF121212),
+            appBar: AppBar(
+              title: const Text("Race Results App"), 
+              backgroundColor: Color.fromARGB(255, 25, 25, 25),
+              centerTitle: true,
+            ),
+          drawer: NavBar(),  
+          body: Field()
+      )
+    );
+  }
+}
 
