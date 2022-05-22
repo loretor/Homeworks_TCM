d9e15d38df45c59c11d31800bc6238451c664f5e
Lorenzo Torri <68181508+loretor@users.noreply.github.com>
2022-05-22 16:47:11 +0200

    Create main.dart

---
+++ Applicazione/main.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import './ListRaces.dart';
+
+import './ButtonRace.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xFF121212),
+        appBar: AppBar(
+          title: const Text("Race Results App"),
+          backgroundColor: Color.fromARGB(255, 25, 25, 25),
+          centerTitle: true,
+        ),
+        body: ListRaces());
+  }
+}
